d0aed83799795a23f83fed2f9095c027ab703956
Aman Narang <aman.narang@nagarro.com>
2024-05-30 17:45:03 +0530

    first commit

---
+++ src/Checklist/types.ts
@@ -0,0 +1,13 @@
+export type ChecklistItemType = {
+    label: string;
+    isChecked: boolean;
+  };
+  
+  export const isChecklistItemType = (value: any): value is ChecklistItemType => {
+    return (
+      typeof value === "object" &&
+      typeof value.label === "string" &&
+      typeof value.isChecked === "boolean"
+    );
+  };
+  
